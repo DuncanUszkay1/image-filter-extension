e70a60d3f3d78461e72aeea448b123a0f4bae5e3
Duncan Uszkay <Duncan.uszkay@hotmail.com>
2019-02-12 16:02:21 -0500

    changed replacement image, threw in a gitignore

--- replaceScript.js
+++ replaceScript.js
@@ -1,4 +1,4 @@
-const REPLACEMENT_IMAGE = 'https://wp-media.patheos.com/blogs/sites/348/2016/02/Put-to-death-therefore.jpg'
+const REPLACEMENT_IMAGE = 'https://pbs.twimg.com/profile_images/1061335246702923776/CKaye4xX_400x400.jpg'
 var imgs = document.getElementsByTagName("img")
 var img_src = [...imgs].map(img => img.src)
 chrome.runtime.sendMessage({imgs: img_src}, function(response) {
